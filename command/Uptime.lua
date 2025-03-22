401409cec3de0fb1ff883e195a23ebf5fe25d807
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2025-03-23 04:44:14 +0530

    Updated shit @oHatless

---
+++ command/Uptime.lua
@@ -0,0 +1,16 @@
+name = "Uptime"
+description = "Tells the time since Minecraft has started use '.uptm' to see."
+author = "Streoids"
+--@Streoids
+local startTime=os.time()
+function onEnable()
+end
+function onDisable()
+end
+function onLoad()
+    registerCommand("uptm", function(args)
+        local elapsed=os.time()-startTime
+        client.notify(string.format("%dh %dm %ds", elapsed//3600,(elapsed%3600)//60,elapsed%60))
+    end)
+end
+ 
